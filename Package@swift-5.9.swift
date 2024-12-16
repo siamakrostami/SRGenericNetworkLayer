4bfcf83d24fd30091bfbba84f565306c70fdabdd
Siamak Rostami <44254012+siamakrostami@users.noreply.github.com>
2024-12-16 20:45:35 +0330

    Create Package@swift-5.9.swift

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "SRNetworkManager",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v7)
+    ],
+    products: [
+        .library(
+            name: "SRNetworkManager",
+            targets: ["SRNetworkManager"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SRNetworkManager",
+            path: "Sources",
+            sources: [
+                "SRNetworkManager",
+                "HeaderHandler",
+                "Encoding",
+                "Log",
+                "Mime",
+                "Error",
+                "Client",
+                "UploadProgress",
+                "Router",
+                "Data"
+            ],
+            swiftSettings: [
+                .define("SPM_SWIFT_5"),
+                .define("SWIFT_PACKAGE")
+            ]
+        ),
+        .testTarget(
+            name: "SRNetworkManagerTests",
+            dependencies: ["SRNetworkManager"],
+            path: "Tests/SRNetworkManagerTests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
